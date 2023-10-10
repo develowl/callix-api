404f06bdcf4d8882a0866320130939a266669b60
Yan Goncalves <fgyan94@gmail.com>
2023-10-10 17:55:12 -0300

    feat: import config and launches modules into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { LaunchesModule } from './launches/launches.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), LaunchesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
