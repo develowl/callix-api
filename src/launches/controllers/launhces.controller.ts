fe98ce200f3e7f9a5d3f63f0b5343c47d3213755
Yan Goncalves <fgyan94@gmail.com>
2023-10-10 18:02:47 -0300

    feat: launches controller should return next launch

--- src/launches/controllers/launhces.controller.ts
+++ src/launches/controllers/launhces.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ILaunch } from '../interfaces/launch.interface';
 import { LaunchesService } from '../services/launches.service';
 
 @Controller('launches')
 export class LaunchesController {
   constructor(private readonly launchesService: LaunchesService) {}
+
+  @Get('next')
+  async getNextLaunch(): Promise<ILaunch> {
+    return this.launchesService.getNextLaunch();
+  }
 }
