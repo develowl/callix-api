21b17d9f6cf062012926d66c6aae43d9f7217449
Yan Goncalves <fgyan94@gmail.com>
2023-10-10 18:51:20 -0300

    refactor: move launches module into a new folder

--- src/launches/modules/launches.module.ts
+++ src/launches/modules/launches.module.ts
@@ -1,7 +1,7 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { LaunchesController } from './controllers/launches.controller';
-import { LaunchesService } from './services/launches.service';
+import { LaunchesController } from '../controllers/launches.controller';
+import { LaunchesService } from '../services/launches.service';
 
 @Module({
   imports: [HttpModule],
