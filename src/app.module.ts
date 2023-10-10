761f469329d3d584ea47a9dceab9161b6575dec1
Yan Goncalves <fgyan94@gmail.com>
2023-10-10 18:51:43 -0300

    refactor: launches module import path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { LaunchesModule } from './launches/launches.module';
+import { LaunchesModule } from './launches/modules/launches.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), LaunchesModule],
   controllers: [],
-  providers: [],
+  providers: []
 })
 export class AppModule {}
