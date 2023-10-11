bb88a7bc6587c5378a14f4a6f3864445b3e10948
Yan Goncalves <fgyan94@gmail.com>
2023-10-10 21:08:12 -0300

    feat: swagger build

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get<number>('PORT', { infer: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Launches API Docs')
+    .setDescription('Documentation for Launches API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(port || 3333);
 }
 bootstrap();
