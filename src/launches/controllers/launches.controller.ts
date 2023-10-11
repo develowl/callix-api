63c736f06bc364c99b2d69a7a1859cfdfd07325e
Yan Goncalves <fgyan94@gmail.com>
2023-10-10 21:14:11 -0300

    feat: add api response in launches controller

--- src/launches/controllers/launches.controller.ts
+++ src/launches/controllers/launches.controller.ts
@@ -1,26 +1,45 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { ILaunch } from '../interfaces/launch.interface';
+import { launchMock } from '../mocks/launch.mock';
 import { LaunchesService } from '../services/launches.service';
 
+@ApiTags('Launches Operations')
 @Controller('launches')
 export class LaunchesController {
   constructor(private readonly launchesService: LaunchesService) {}
 
+  @ApiOkResponse({
+    description: 'Get next launch',
+    schema: { example: launchMock }
+  })
   @Get('next')
   async getNextLaunch(): Promise<ILaunch> {
     return this.launchesService.getNextLaunch();
   }
 
+  @ApiOkResponse({
+    description: 'Get latest launch',
+    schema: { example: launchMock }
+  })
   @Get('latest')
   async getLatestLaunch(): Promise<ILaunch> {
     return this.launchesService.getLatestLaunch();
   }
 
+  @ApiOkResponse({
+    description: 'Get upcoming launches',
+    schema: { example: [launchMock] }
+  })
   @Get('upcoming')
   async getUpcomingLaunch(): Promise<ILaunch[]> {
     return this.launchesService.getUpcomingLaunches();
   }
 
+  @ApiOkResponse({
+    description: 'Get past launches',
+    schema: { example: [launchMock] }
+  })
   @Get('past')
   async getPastLaunch(): Promise<ILaunch[]> {
     return this.launchesService.getPastLaunches();
