6f6299026d206e9abda17bb69aa50fbd4ee8a995
Yan Goncalves <yangoncalves@develowl.com.br>
2023-10-12 11:56:08 -0300

    refactor: remove header api key

--- src/launches/modules/launches.module.ts
+++ src/launches/modules/launches.module.ts
@@ -4,13 +4,7 @@ import { LaunchesController } from '../controllers/launches.controller';
 import { LaunchesService } from '../services/launches.service';
 
 @Module({
-  imports: [
-    HttpModule.register({
-      headers: {
-        'spacex-key': 'b163f2e0-8d9d-401b-aa34-fce356ffaa44'
-      }
-    })
-  ],
+  imports: [HttpModule],
   controllers: [LaunchesController],
   providers: [LaunchesService]
 })
